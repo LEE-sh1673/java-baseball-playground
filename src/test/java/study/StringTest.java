bf267feb2b82abd072ba38871240f704cbf5d3ed
LEE-sh1673 <lsh901673@gmail.com>
2022-08-08 18:12:12 +0900

    feat($common): add unit test of `StringTest.java`
    add unit test of `contains()` and `replace()` to test for contain and
    replace element correctly.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,39 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
-    @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
-    }
+	@Test
+	void replace() {
+		String actual = "abc".replace("b", "d");
+		assertThat(actual).isEqualTo("adc");
+	}
+
+	@Test
+	void split() {
+		String[] values = "1".split(",");
+		assertThat(values).contains("1");
+		assertThat(values.length).isEqualTo(1);
+		values = "1,2".split(",");
+		assertThat(values).containsExactly("1", "2");
+	}
+
+	@Test
+	void charAt() {
+		String value = "abc";
+		char c = value.charAt(0);
+		assertThat(c).isEqualTo('a');
+
+		assertThatThrownBy(() -> value.charAt(value.length()))
+			.isInstanceOf(StringIndexOutOfBoundsException.class);
+	}
+
+	@Test
+	void substring() {
+		String input = "(1,2)";
+		String substring = input.substring(1, input.length() - 1);
+		assertThat(substring).isEqualTo("1,2");
+	}
 }
