f0583de51b73203e0954ef6cfadaa03efe65c7c6
LEE-sh1673 <lsh901673@gmail.com>
2022-08-10 16:31:14 +0900

    feat($common): 1:1 숫자 비교 기능 추가 (NOTHING)

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,16 @@
+package baseball;
+
+public class Ball {
+	private final int number;
+
+	private final int position;
+
+	public Ball(int number, int position) {
+		this.number = number;
+		this.position = position;
+	}
+
+	public BallStatus play(final Ball ball) {
+		return BallStatus.NOTHING;
+	}
+}
---
+++ src/test/java/baseball/BallTest.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BallTest {
+
+	Ball ball;
+
+	@BeforeEach
+	void setUp() {
+		ball = new Ball(1, 1);
+	}
+
+	@DisplayName("1:1 숫자 비교::낫싱")
+	@Test
+	void should_get_nothing() {
+		BallStatus status = ball.play(new Ball(2, 5));
+		assertThat(status).isEqualTo(BallStatus.NOTHING);
+	}
+}
