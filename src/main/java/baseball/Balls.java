cc1c6231945e32dca9fca82b6ae2c8b42c0fc03d
LEE-sh1673 <lsh901673@gmail.com>
2022-08-10 15:40:04 +0900

    feat($common): N:M 도메인 집합 비교 기능 추가

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Balls {
 
-	private final List<Ball> balls;
+	private final List<Ball> answers;
 
 	public Balls(List<Integer> numbers) {
-		this.balls = mapBall(numbers);
+		this.answers = mapBall(numbers);
 	}
 
 	private static List<Ball> mapBall(List<Integer> numbers) {
@@ -19,11 +19,22 @@ public class Balls {
 		return balls;
 	}
 
-	public BallStatus play(final Ball userBall) {
-		return balls.stream()
+	BallStatus play(final Ball userBall) {
+		return answers.stream()
 			.map(ball -> ball.play(userBall))
 			.filter(BallStatus::isNotNothing)
 			.findFirst()
 			.orElse(BallStatus.NOTHING);
+	}
+
+	public PlayResult play(final List<Integer> balls) {
+
+		Balls userBalls = new Balls(balls);
+		PlayResult result = new PlayResult();
+
+		for (final Ball ball : answers) {
+			result.report(userBalls.play(ball));
+		}
+		return result;
 	}
 }
--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -17,6 +17,30 @@ public class BallsTest {
 		balls = new Balls(Arrays.asList(1, 2, 3));
 	}
 
+	@DisplayName("N:M 검사::NOTHING")
+	@Test
+	void should_get_all_nothing() {
+		PlayResult result = balls.play(Arrays.asList(4, 5, 6));
+		assertThat(result.getStrike()).isEqualTo(0);
+		assertThat(result.getBall()).isEqualTo(0);
+	}
+
+	@DisplayName("N:M검사::1 스트라이크 1볼")
+	@Test
+	void should_get_one_strike_one_ball() {
+		PlayResult result = balls.play(Arrays.asList(1, 4, 2));
+		assertThat(result.getStrike()).isEqualTo(1);
+		assertThat(result.getBall()).isEqualTo(1);
+	}
+	@DisplayName("N:M검사::3 스트라이크")
+	@Test
+	void should_get_three_strike() {
+		PlayResult result = balls.play(Arrays.asList(1, 2, 3));
+		assertThat(result.getStrike()).isEqualTo(3);
+		assertThat(result.getBall()).isEqualTo(0);
+		assertThat(result.isGameEnd()).isTrue();
+	}
+
 	@DisplayName("요소 검사::ONE_NOTHING")
 	@Test
 	void should_get_nothing() {
